29658824464a5e2f4c908dc2872fef9637a84c9d
1º DAM 4v - Egoitz Trabudua <egoitz_trabudua@cuatrovientos.es>
2018-11-19 13:15:07 +0100

    Clases Estante, Estanteria, Marca y producto

---
+++ proyectoFPBasica/Entidades/Estanteria.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Estanteria
+    {
+        public int IdEstante { get; set; }
+
+        public int NumAltura { get; set; }
+
+        public int NumEstante { get; set; }
+
+        public Estanteria()
+        {
+        }
+
+        public Estanteria(int idEstante)
+        {
+            IdEstante = idEstante;
+        }
+
+        public Estanteria(int idEstante, int numAltura, int numEstante) : this(idEstante)
+        {
+            NumAltura = numAltura;
+            NumEstante = numEstante;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var estanteria = obj as Estanteria;
+            return estanteria != null &&
+                   IdEstante == estanteria.IdEstante;
+        }
+
+        public override int GetHashCode()
+        {
+            return -411862092 + IdEstante.GetHashCode();
+        }
+
+
+    }
+}
---
+++ proyectoFPBasica/Entidades/Producto.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Producto
+    {
+
+        public string CodProducto { get; set; }
+        public string CodBarras { get; set; }
+        public string Descripcion { get; set; }
+        public int IdMarca { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+        public int PesoNeto { get; set; }
+        public int PesoNuto { get; set; }
+
+        public Producto()
+        {
+        }
+
+        public Producto(string codProducto)
+        {
+            CodProducto = codProducto;
+        }
+
+        public Producto(string codProducto, string codBarras, string descripcion, int idMarca, decimal precio, int stock, int pesoNeto, int pesoNuto) : this(codProducto)
+        {
+            CodBarras = codBarras;
+            Descripcion = descripcion;
+            IdMarca = idMarca;
+            Precio = precio;
+            Stock = stock;
+            PesoNeto = pesoNeto;
+            PesoNuto = pesoNuto;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var producto = obj as Producto;
+            return producto != null &&
+                   CodProducto == producto.CodProducto;
+        }
+
+        public override int GetHashCode()
+        {
+            return -1048562515 + EqualityComparer<string>.Default.GetHashCode(CodProducto);
+        }
+    }
+}
