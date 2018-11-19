82be0b414be709417f8a86d21eabd945b7648523
Javier97PRnorth <Javier97PRnorth@users.noreply.github.com>
2018-11-19 13:07:04 +0100

    V1: Creación de la sección de clases: TipoDeSeguro, Cliente, Empresa. (Javier Pérez)

---
+++ proyectoFPBasica/Entidades/Cliente.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Cliente : IEquatable<Cliente>
+    {
+        public string Nif { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string LugarEntrega { get; set; }
+
+        public Cliente()
+        {
+        }
+
+        public Cliente(string nif)
+        {
+            Nif = nif;
+        }
+
+        public Cliente(string nif, string nombre, string direccion, string lugarEntrega) : this(nif)
+        {
+            Nombre = nombre;
+            Direccion = direccion;
+            LugarEntrega = lugarEntrega;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Cliente);
+        }
+
+        public bool Equals(Cliente other)
+        {
+            return other != null &&
+                   Nif == other.Nif;
+        }
+
+        public override int GetHashCode()
+        {
+            return -146653734 + EqualityComparer<string>.Default.GetHashCode(Nif);
+        }
+    }
+}
---
+++ proyectoFPBasica/Entidades/Empresa.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Empresa : IEquatable<Empresa>
+    {
+        public string Nif { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Logo { get; set; }
+
+        public Empresa()
+        {
+        }
+
+        public Empresa(string nif)
+        {
+            Nif = nif;
+        }
+
+        public Empresa(string nif, string nombre, string direccion, string logo) : this(nif)
+        {
+            Nombre = nombre;
+            Direccion = direccion;
+            Logo = logo;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Empresa);
+        }
+
+        public bool Equals(Empresa other)
+        {
+            return other != null &&
+                   Nif == other.Nif;
+        }
+
+        public override int GetHashCode()
+        {
+            return -146653734 + EqualityComparer<string>.Default.GetHashCode(Nif);
+        }
+    }
+}
