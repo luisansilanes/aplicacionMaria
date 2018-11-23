9d102cd88a859ecc2152ccd0c42ef8db01c161ee
Javier97PRnorth <Javier97PRnorth@users.noreply.github.com>
2018-11-23 11:07:07 +0100

    V1: añadiendo gestores (Javier Pérez)

---
+++ proyectoFPBasica/CapaDatos/GestorCliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class GestorCliente
+    {
+
+        string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=AlmacenesPELA;";
+
+    }
+}
