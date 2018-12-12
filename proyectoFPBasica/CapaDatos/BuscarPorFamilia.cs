f654bd573bfaccb8fc68b4ec336f054b88ecc5a4
Javier97PRnorth <Javier97PRnorth@users.noreply.github.com>
2018-12-12 14:14:25 +0100

    V1: jodeeeeeeeeeeeer

--- proyectoFPBasica/CapaDatos/BuscarPorFamilia.cs
+++ proyectoFPBasica/CapaDatos/BuscarPorFamilia.cs
@@ -14,11 +14,11 @@ namespace CapaDatos
     {
 
         /// <summary>
-        /// Método para añadir una Empresa. Si todo sale bien devuelve un String "vacio"... Si no, mostrará un mensaje de error.
+        /// Método para buscar un Producto y su a través de su Familia. Si todo sale bien devuelve un String "vacio"... Si no, mostrará un mensaje de error.
         /// </summary>
         /// <param name="empresa"></param>
         /// <returns></returns>
-        public string Buscar(string nombre)
+        public List<Producto> Buscar(string nombre)
         {
             MySqlConnectionStringBuilder builder = this.StringConexion();
             using (MySqlConnection conexion = new MySqlConnection(builder.ToString()))
@@ -31,7 +31,17 @@ namespace CapaDatos
                     cmdBuscarIdFamilia.Parameters.AddWithValue("@Nombre", nombre);
                     MySqlDataReader drBuscarIdFamilia = cmdBuscarIdFamilia.ExecuteReader();
 
-                    if (!drBuscarIdFamilia.Read()) return "No se ha podido encontrar la familia...";
+                    if (!drBuscarIdFamilia.Read()) return null;
+
+                    // Ahora que tenemos el id de la familia, buscamos en el código de barras
+                    string sqlBuscarProductoConIdFamilia = "SELECT * From Producto WHERE CodBarras LIKE _______@IdFamilia%";
+                    MySqlCommand cmdBuscarProductoConIdFamilia = new MySqlCommand(sqlBuscarProductoConIdFamilia, conexion);
+                    cmdBuscarProductoConIdFamilia.Parameters.AddWithValue("@IdFamilia", nombre);
+                    MySqlDataReader drBuscarProductoConIdFamilia = cmdBuscarProductoConIdFamilia.ExecuteReader();
+
+                    if (!drBuscarIdFamilia.Read()) return null;
+
+
                 }
                 catch (Exception e)
                 {
